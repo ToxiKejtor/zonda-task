91c9eb6063c8feb8af5fd18cb4f7f365ea4f6707
Maciej Tokarczyk <maciek.tokarczyk1@gmail.com>
2023-02-12 23:32:13 +0100

    Add loading state to Spread component

--- src/components/Spread.vue
+++ src/components/Spread.vue
@@ -1,12 +1,28 @@
 <template>
   <v-banner lines="one">
-    <div class="text-center">Spread: {{ orderbook.spread }}</div>
+    <AnimatedPlaceholder v-if="loading" height="20px" />
+    <div v-else class="text-center">Spread: {{ spread }}</div>
   </v-banner>
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from "vue";
 import { useOrderbook } from "@/stores/useOrderbook";
+import { useCurrencyPairs } from "@/stores/useCurrencyPairs";
+import AnimatedPlaceholder from "@/components/AnimatedPlaceholder.vue";
+import { storeToRefs } from "pinia";
 const orderbook = useOrderbook();
+const currencyPairs = useCurrencyPairs();
+let loading = ref(true);
+const { currentPair } = storeToRefs(currencyPairs);
+const { spread } = storeToRefs(orderbook);
+watch(currentPair, () => {
+  loading.value = true;
+});
+
+watch(spread, () => {
+  loading.value = false;
+});
 </script>
 
 <style scoped></style>
